a85ac1df3488ea1b608effd3e1a88d235098448b
cedric karungu <ckarungu921@gmail.com>
2022-03-17 20:20:53 +0200

    creation of the ThemeContext

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,18 @@
+import React, { Component, createContext } from "react";
+
+export const ThemeContext = createContext();
+
+class ThemeContextProvider extends Component {
+   state = {
+      isLightTheme: true,
+      light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
+      dark: { syntax: '#ddd', ui: '#333', bg: '#555' }
+   }
+  render() {
+     return (
+        <ThemeContext.Provider value={{...this.state}}>
+           {this.props.children}
+       </ThemeContext.Provider>
+    );
+  }
+}
