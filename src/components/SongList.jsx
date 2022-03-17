f9914b83a089983f558617d85784c8ba381e8689
cedric karungu <ckarungu921@gmail.com>
2022-03-17 19:35:46 +0200

    creation of the SongList function component

---
+++ src/components/SongList.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const SongList = () => {
+  useState([])
+  return (
+    <div className='song-list'>
+      <ul>
+        <li>this wild darkness</li>
+        <li>memory gospel</li>
+      </ul>
+    </div>
+  )
+}
+
+export default SongList
