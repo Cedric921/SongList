545924fa3597f58d4aca86d1d2abbf979604a3ed
cedric karungu <ckarungu921@gmail.com>
2022-03-17 21:12:22 +0200

    ThemeContext Consummer in Navbar

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className='App'>
       <ThemeContextProvider>
-        <BookList />
         <Navbar />
+        <BookList />
       </ThemeContextProvider>
     </div>
   )
