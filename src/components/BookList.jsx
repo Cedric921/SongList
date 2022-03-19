85d6eaa4afdec1007722036983124da4fd9c9540
cedric karungu <ckarungu921@gmail.com>
2022-03-19 17:09:14 +0200

    change the BookList from class component to function component

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,29 +1,21 @@
-import React, { Component } from 'react'
-import { ThemeContext } from '../contexts/ThemeContext'
+import React, { useContext } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context
-          const theme = isLightTheme ? light : dark
-          return (
-            <div
-              className='book-list'
-              style={{ color: theme.syntax, background: theme.bg }}
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>the way of king</li>
-                <li style={{ background: theme.ui }}>the name of the wind</li>
-                <li style={{ background: theme.ui }}>the final empire</li>
-              </ul>
-            </div>
-          )
-        }}
-      </ThemeContext.Consumer>
-    )
-  }
+const BookList = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark
+  return (
+    <div
+      className='book-list'
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>the way of king</li>
+        <li style={{ background: theme.ui }}>the name of the wind</li>
+        <li style={{ background: theme.ui }}>the final empire</li>
+      </ul>
+    </div>
+  )
 }
 
 export default BookList
