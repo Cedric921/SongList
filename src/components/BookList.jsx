7cee31867460e267d9fdef511aff9700d76ae911
cedric karungu <ckarungu921@gmail.com>
2022-03-19 16:14:10 +0200

    ThemeContext Consummer in BookList

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -7,16 +7,22 @@ class BookList extends Component {
     const { isLightTheme, light, dark } = this.context
     const theme = isLightTheme ? light : dark
     return (
-      <div
-        className='book-list'
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>the way of king</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final empire</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {(context) => {
+          return (
+            <div
+              className='book-list'
+              style={{ color: theme.syntax, background: theme.bg }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>the way of king</li>
+                <li style={{ background: theme.ui }}>the name of the wind</li>
+                <li style={{ background: theme.ui }}>the final empire</li>
+              </ul>
+            </div>
+          )
+        }}
+      </ThemeContext.Consumer>
     )
   }
 }
