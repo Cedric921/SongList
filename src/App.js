a6729a5fe8c1db7b0acff95147c67b35b0b8693c
cedric karungu <ckarungu921@gmail.com>
2022-03-19 16:44:15 +0200

    add the ToggleTheme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BookList from './components/BookList'
 import Navbar from './components/Navbar'
+import ThemeToggle from './components/ThemeToggle'
 import ThemeContextProvider from './contexts/ThemeContext'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <ThemeContextProvider>
         <Navbar />
         <BookList />
+        <ThemeToggle />
       </ThemeContextProvider>
     </div>
   )
