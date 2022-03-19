6028cd4350fd23b1f986fe6de63839841704e30c
cedric karungu <ckarungu921@gmail.com>
2022-03-19 17:19:16 +0200

    change the ToggleTheme from class component to function component

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,13 +1,9 @@
-import React, { Component } from "react";
-import { ThemeContext } from "../contexts/ThemeContext";
+import React, { useContext } from 'react'
+import { ThemeContext } from '../contexts/ThemeContext'
 
-export default class ThemeToggle extends Component {
-   static contextType = ThemeContext;
-
-   render() {
-      const { toggleTheme } = this.context; 
-     return (
-       <button onClick={toggleTheme}>Toggle Theme</button>
-    );
-  }
+const ThemeToggle = () => {
+  const { toggleTheme } = useContext(ThemeContext)
+  return <button onClick={toggleTheme}>Toggle Theme</button>
 }
+
+export default ThemeToggle
