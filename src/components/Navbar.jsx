9f88615ba597957a2e161f958056aa4f5f46191d
cedric karungu <ckarungu921@gmail.com>
2022-03-19 17:13:37 +0200

    change the NavBar from class component to function component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component, useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
-class Navbar extends Component {
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context
-          const theme = isLightTheme ? light : dark
-          return (
-            <nav style={{ background: theme.ui, color: theme.syntax }}>
-              <h1>Context App</h1>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          )
-        }}
-      </ThemeContext.Consumer>
-    )
-  }
+const Navbar = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext)
+  const theme = isLightTheme ? light : dark
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
