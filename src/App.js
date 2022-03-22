dcbfac3dbe0ae481f9b7f163a87a66551b700ec7
cedric karungu <ckarungu921@gmail.com>
2022-03-22 17:08:04 +0200

    add style to the booklist

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import BookList from './components/BookList'
 import Navbar from './components/Navbar'
 import BookContextProvider from './contexts/BookContext'
 
@@ -7,6 +8,7 @@ function App() {
     <div className='App'>
       <BookContextProvider>
         <Navbar />
+        <BookList />
       </BookContextProvider>
     </div>
   )
